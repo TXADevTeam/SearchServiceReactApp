06a1ae8e65e23ec4cd6755dce6df7bfc82a81202
Nurzhanov Rashit <7119967@mail.ru>
2020-11-24 22:20:45 +0600

    Добавил главную страницу

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Jumbotron, Container, Row, Col, Button, InputGroup, FormControl, Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NaviBar from './Components/Navbar';
+
+import { Home } from './Home';
+// import {Users} from './Users';
+// import {About} from './About';
 
 function App() {
+  
+  const handleSearch = () => (
+    alert('you clicked me')
+
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NaviBar />
+        <Container style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
+          <Row>
+            <h1>Search Service</h1>
+          </Row>
+        </Container>
+        <Switch>
+          {/* <Route exact path="/" component={Home} /> */}
+          {/* <Route path="/users" component={Users} />
+          <Route path="/about" component={About} /> */}
+        </Switch>
+      </Router>
+      <Container>
+        <Jumbotron>
+          <p>
+            <InputGroup>
+              <FormControl
+                placeholder="Введите строку поиска"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+            </InputGroup>
+          </p>
+          <p>
+            <Button variant="primary" onClick={handleSearch}>Поиск</Button>
+          </p>
+        </Jumbotron>
+      </Container>
+      <Container>
+        <Table bordered hover>
+          <thead className="thead-light">
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    </>
   );
 }
 
---
+++ react-ts-app/src/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+export const Home = () => (
+    <>
+{/* 
+        <Container style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
+            <Row>
+                <h1>UI Search Service</h1>
+            </Row>
+        </Container> */}
+    </>
+
+)
